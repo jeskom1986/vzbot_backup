012dde1e337fff5913b9b19b48120d85676070b7
jeskom1986 <mjesko1@gmail.com>
2023-12-20 22:17:54 +0100

    Autocommit from 2023-12-20 22:17:54
    Klipper on commit: 3417940
    
    Moonraker on commit: 4235789
    
    Mainsail version: v2.9.0

--- autocommit.sh
+++ autocommit.sh
@@ -77,8 +77,8 @@ push_config(){
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git checkout -b master
-  git push origin master origin/master
+  git checkout master
+  git push origin master 
 }
 
 grab_version
